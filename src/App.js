2aebd04dbb7cb2d56c281d22712c22cf61438b76
kinotonik <zedmed@kinotonik.fr>
2022-06-02 21:44:44 +0200

    ras

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Routes, Route} from "react-router-dom"
 import Header from './components/templates/Header';
 import Home from './screen/Home';
 import Goodies from './screen/Goodies';
-
+import Footer from './components/templates/Footer';
 
 
 
@@ -14,7 +14,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/goodies' element={ <Goodies />} />
-    </Routes>
+      </Routes>
+
   </div>
   );
 }
