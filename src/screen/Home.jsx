61b4a4d3dea7a9140023277329b95abb7d0519d4
kinotonik <zedmed@kinotonik.fr>
2022-06-03 07:30:36 +0200

    Add new components->truelover + modif header

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -2,14 +2,16 @@ import Header from '../components/templates/Header'
 import Main from '../components/templates/Main'
 import Footer from '../components/templates/Footer'
 import Forms from '../components/Forms'
-
 import './Home.css'
 import CardsList from '../components/CardsList'
+
+
 const Home = () => {
     return(
 
         <div className="home app-container">
             <Header className="header"/>
+
             <Main  className="main" >
              {/* (formsValidate) && <Forms/>  */}
                <CardsList/>
