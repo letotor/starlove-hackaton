2cead0599ebaf3fa8805ba435039e499d06db50b
kirijeremy <kirijeremy@gmail.com>
2022-06-03 14:14:38 +0200

    remove font awsome

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import './CardDetail.css'
-import 'font-awesome/css/font-awesome.min.css';
+// import 'font-awesome/css/font-awesome.min.css';
 import { Link } from 'react-router-dom';
 
 const CardDetail = ({id}) => {
